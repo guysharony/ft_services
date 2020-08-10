99c46298b1c974710a492ea76c88bdef9063cd10
sharonyguy <sharonyguy@gmail.com>
2020-08-10 09:43:04 +0200

    Finished

--- srcs/images/wordpress/srcs/setup.sh
+++ srcs/images/wordpress/srcs/setup.sh
@@ -3,7 +3,7 @@ apk add php7 php7-fpm php7-opcache
 apk add php7-gd php7-mysqli php7-zlib php7-curl php7-mbstring php7-session php7-json php7-mbstring
 apk add mysql
 apk add wget
-wget https://wordpress.org/latest.tar.gz
+
 tar -xzf latest.tar.gz
 rm -rf latest.tar.gz
 mv wordpress ./www
