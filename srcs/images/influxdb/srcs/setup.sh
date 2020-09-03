ca2eee45a31f9848f60bb612dc5de8b9143d3f00
user42 <sharonyguy@gmail.com>
2020-09-03 14:40:39 +0200

    Adding telegraf for influxdb

--- srcs/images/influxdb/srcs/setup.sh
+++ srcs/images/influxdb/srcs/setup.sh
@@ -1,6 +1,2 @@
-apk add influxdb
-
-influxd restore -metadir /var/lib/influxdb/meta ./data
-influxd restore -database grafana -datadir /var/lib/influxdb/data ./data
-
-influxd run -config /etc/influxdb.conf
+influxd run -config /etc/influxdb.conf &> /dev/null
+telegraf
